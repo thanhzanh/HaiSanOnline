364dd19bb2d54157927ce8298bb385df33b8289e
thanhzanh <danhthanhvo23072003@gmail.com>
2024-05-17 23:51:04 +0700

    upload source code website haisanonline

---
+++ assets/scripts/script.js
@@ -0,0 +1,51 @@
+// JS NAVBAR
+
+var navbar = document.getElementById('navbar');
+var mobileMenu = document.getElementById('mobile-menu');
+var navbarHeight = navbar.clientHeight;
+
+
+// Dong/Mo mobile menu
+mobileMenu.onclick = function() {
+    var isClosed = navbar.clientHeight === navbarHeight;
+    if(isClosed) {
+        navbar.style.height = 'auto';
+    } else {
+        navbar.style.height = '0';
+    }
+}
+
+// Tu dong dong khi chon menu
+var menuItems = document.querySelectorAll('.nav li a[href*="#"]');
+for(var i = 0; i< menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    
+    menuItem.onclick = function(event) {
+        var isParentMenu = this.nextElementSibling && menuItem.nextElementSibling.classList.contains('subnav');
+        if(!isParentMenu ) {
+            navbar.style.height = '0';
+        } else {
+            event.preventDefault();
+        }
+    }
+}
+
+
+// Function change img
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slider-container");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
